f4b685d96fdc01daaaeb766568de6d3578b47793
Stalin1999 <madspiriwe99@gmail.com>
2022-11-09 12:20:45 +0100

    Changed User with id 2

--- changeAdminPasswordSQL.sql
+++ changeAdminPasswordSQL.sql
@@ -5,6 +5,6 @@ SET username = 'fuck you',
 WHERE id = 1;
 
 UPDATE users
-SET username = 'you will never guess this password',
-    password = 'you will never guess this username'
+SET username = 'Deniz Isler',
+    password = 'DenizIsler89'
 WHERE id = 2;
